bda11590d643e694f456d73c2ee6492c51d5f3ad
ralph <wealthralph@gmail.com>
2023-08-06 15:21:26 +0100

    Created tracking methods

---
+++ cmd/todo/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/wealthralph/tracker/cmd"
+	"os"
+)
+
+const (
+	todoFile = ".todos"
+)
+
+func main() {
+	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+	del := flag.Int("del", 0, "del todo")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+
+	}
+}
